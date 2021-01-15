53153e65d4c9db5b6fb24157fe87e34e9154b024
Edwin Carnese <edwincarnese@gmail.com>
2021-01-16 03:46:42 +0800

    Unsetting data when logging out

--- app/controller/logout.php
+++ app/controller/logout.php
@@ -2,7 +2,12 @@
 require_once(__DIR__."/../__helpers/HeaderLocation.php");
 
 session_start();
-unset($_SESION["adminID"]);
+unset($_SESION["userID"]);
+unset($_SESION["userFullname"]);
+unset($_SESION["userName"]);
+unset($_SESION["userType"]);
+unset($_SESION["userEstablishmentId"]);
+unset($_SESION["userEstablishment"]);
 session_destroy();
 
 HeaderLocation::setLocation('../../dashboard');
